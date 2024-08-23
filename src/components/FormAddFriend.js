f0470c73fdeaf112b6279c0f313a00a3e82a8d7f
rendrazuriansyah <rendra.zurd04@gmail.com>
2024-08-23 21:58:01 +0700

    Added form add friend and split bill components for splitting bills

---
+++ src/components/FormAddFriend.js
@@ -0,0 +1,16 @@
+export default function FormAddFriend() {
+	return (
+		<form
+			action=""
+			className="form-add-friend"
+		>
+			<label htmlFor="">🙍🏻Nama</label>
+			<input type="text" />
+
+			<label htmlFor="">📸Foto</label>
+			<input type="text" />
+
+			<button className="button">Tambah</button>
+		</form>
+	);
+}
---
+++ src/components/FormSplitBill.js
@@ -0,0 +1,43 @@
+export default function FormSplitBill() {
+	return (
+		<form
+			action=""
+			className="form-split-bill"
+		>
+			<h2>Patungan Bareng si X</h2>
+
+			<label htmlFor="">💵Total Tagihan</label>
+			<input
+				type="text"
+				name=""
+				id=""
+			/>
+
+			<label htmlFor="">🙋🏻‍♂️Tagihan Kamu</label>
+			<input
+				type="text"
+				name=""
+				id=""
+			/>
+
+			<label htmlFor="">🙋🏻Tagihan X</label>
+			<input
+				type="text"
+				name=""
+				id=""
+				disabled
+			/>
+
+			<label htmlFor="">🤑Dilunasin sama</label>
+			<select
+				name=""
+				id=""
+			>
+				<option value="user">Kamu</option>
+				<option value="friend">X</option>
+			</select>
+
+			<button className="button">Tambah</button>
+		</form>
+	);
+}
